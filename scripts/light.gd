6e3fbb911bd6773e43950cc8701563660dd53c23
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-18 00:01:41 +0400

    feat: Add simple halo to inter. light class

--- scripts/light.gd
+++ scripts/light.gd
@@ -17,28 +17,60 @@ signal reignited
 			extinguished.emit()
 
 @export_group("Intensity")
-@export var intensity := 4.0:
+@export var intensity := 3.0:
 	set(value):
+		var before = intensity
+		
 		intensity = clamp(value, min_intensity, max_intensity)
-		intensity_changed.emit(intensity)
-@export var min_intensity := 1.0
-@export var max_intensity := 6.0
-@export var energy_multiplier := 0.3
-@export var radius_multiplier := 2.7
+		
+		if intensity != before:
+			intensity_changed.emit(intensity)
+@export var min_intensity := 1.0:
+	set(value):
+		min_intensity = value
+		intensity = intensity
+@export var max_intensity := 6.0:
+	set(value):
+		max_intensity = value
+		intensity = intensity
+@export var energy_multiplier := 0.25:
+	set(value):
+		energy_multiplier = value
+		_update()
+@export var radius_multiplier := 1.5:
+	set(value):
+		radius_multiplier = value
+		_update()
 
 @export_group("Transition")
-@export var intensity_decrease_speed := 5.0
-@export var intensity_increase_speed := 6.0
+@export var intensity_transition_speed := 6.0
 var _current_intensity := intensity:
 	set(value):
 		_current_intensity = value
 		_update()
 var _intensity_snap_threshold := 0.005
 
+@export_group("Halo")
+@export var create_halo := false:
+	set(value):
+		create_halo = value
+		if create_halo: _create_halo()
+		else: _remove_halo()
+@export var halo_energy_multiplier := 0.25
+@export var halo_radius_multiplier := 0.75
+@export var halo_fog_multiplier := 3.0
+var _halo: OmniLight3D
+
 func _update():
 	var lit_multiplier := 1 if is_lit else 0
+	
 	light_energy = _current_intensity * energy_multiplier * lit_multiplier
 	omni_range = _current_intensity * radius_multiplier
+	
+	if _halo:
+		_halo.light_energy = _current_intensity * halo_energy_multiplier
+		_halo.omni_range = _current_intensity * halo_radius_multiplier
+		_halo.light_volumetric_fog_energy = _current_intensity * halo_fog_multiplier
 
 func _transition(delta: float):
 	if not is_lit: return
@@ -48,16 +80,27 @@ func _transition(delta: float):
 		_current_intensity = intensity
 		return
 	
-	var transition_speed = (
-		intensity_increase_speed if _current_intensity < intensity
-		else intensity_decrease_speed
-	)
-	
 	_current_intensity = lerp(_current_intensity,
-		intensity, delta * transition_speed)
+		intensity, delta * intensity_transition_speed)
+
+func _create_halo():
+	if _halo: return
+	
+	_halo = OmniLight3D.new()
+	_halo.light_color = light_color
+	add_child(_halo)
+	
+	_update()
+
+func _remove_halo():
+	if not _halo: return
+	
+	_halo.queue_free()
+	_halo = null
 
 func _ready():
 	intensity = intensity
+	if create_halo: _create_halo()
 	_update()
 
 func _process(delta: float):
