009dc5722c5b5cb99224c60385b8dacefaf0754f
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-19 15:54:47 +0400

    feat: Add fuel & depletion to lantern class

--- scripts/player/lantern.gd
+++ scripts/player/lantern.gd
@@ -1,14 +1,52 @@
 extends Node
 
 @export var light: InteractiveLight
-@export var intensity_scroll_speed := 0.25
+
+@export_group("General")
+@export var intensity_change_rate := 0.45
+@export var update_rate := 1.0
+var update_timer := 0.0
+
+@export_group("Fuel")
+@export var fuel := 75.0:
+	set(value):
+		fuel = clampf(value, 0, fuel_limit)
+		if fuel == 0:
+			light.is_lit = false
+@export var fuel_limit := 100.0
+@export var fuel_deplition_rate := 3.5
+
+func lightup(intensity: float = -1.0):
+	light.is_lit = true
+	if intensity > 0: light.intensity = intensity
+
+func extinguish():
+	light.is_lit = false
+
+func change_intensity(dir: float, strength: float = intensity_change_rate) -> float:
+	light.intensity += dir * strength
+	return light.intensity
+
+func deplete(how_much: float = fuel_deplition_rate):
+	if not light.is_lit: return
+	fuel -= how_much
+	print('LANTERN F:',fuel,'%, DR:',fuel_deplition_rate)
+
+func update():
+	deplete()
 
 func _unhandled_input(event: InputEvent):
 	if event.is_action_pressed('light'):
 		light.is_lit = not light.is_lit
 	
 	elif event.is_action_pressed('light_up'):
-		light.intensity += intensity_scroll_speed
-	
+		change_intensity(1)
 	elif event.is_action_pressed('light_down'):
-		light.intensity -= intensity_scroll_speed
+		change_intensity(-1)
+
+func _process(delta: float):
+	if update_timer > 0:
+		update_timer -= delta
+	else:
+		update()
+		update_timer = update_rate
