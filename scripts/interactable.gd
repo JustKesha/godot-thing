507fb67a67dd43346863d9e14d6515b27e667798
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-22 05:18:57 +0400

    feat: Add base Interactable class

---
+++ scripts/interactable.gd
@@ -0,0 +1,6 @@
+class_name Interactable extends StaticBody3D
+
+func hover(): push_warning("Hover not implemented in: ", name)
+func unhover(): push_warning("Unhover not implemented in: ", name)
+func interact(player: PlayerAPI):
+	push_warning("Interact not implemented in: ", name)
