461a0f16a7b5562d3270fceffb6e020f6a3e3ee4
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-22 19:30:43 +0400

    feat: Add allow_overflow to Pickup Interactable

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -2,11 +2,12 @@ class_name Pickup extends Interactable
 
 @export var item_name := 'matches'
 @export var quantity := 1
+@export var allow_overflow := false
 
 func interact(player: PlayerAPI):
 	var overflow := player.inventory.add(item_name, quantity)
 	
-	if overflow == 0:
+	if overflow == 0 or allow_overflow:
 		queue_free()
 	else:
 		quantity = overflow
