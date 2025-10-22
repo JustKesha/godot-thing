4a1c95ca4cb0ba81f00a90e83f8b06d746a86dd1
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-22 05:20:57 +0400

    feat: Create min script for player interactions

---
+++ scripts/player/hands.gd
@@ -0,0 +1,21 @@
+class_name PlayerHands extends RayCast3D
+
+@export var player: PlayerAPI
+
+var hovered: Interactable = null:
+	set(value):
+		if hovered == value: return
+		if hovered != null: hovered.unhover()
+		
+		if value is Interactable:
+			hovered = value
+			hovered.hover()
+		else:
+			hovered = null
+
+func _process(delta: float):
+	hovered = get_collider()
+
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed('interact'):
+		if hovered: hovered.interact(player)
