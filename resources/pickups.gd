d5919240d849616bcc5ecd1deff0c8facb46e50b
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-25 22:38:07 +0400

    chore: Move resource scripts into a separate dir

--- resources/pickups.gd
+++ resources/pickups.gd
@@ -1,20 +1,15 @@
 class_name Pickups extends Resource
 
+static var path := "res://scenes/objects/pickups/"
+static var comm_prefix := "comm_"
 static var comm_chance := -1.0
-static var comm: Array[Resource] = [
-	preload("res://scenes/objects/pickups/candle.tscn"),
-]
+static var comm: Array[String] = []
 static var rare_prefix := "rare_"
 static var rare_chance := 30.0
-static var rare: Array[Resource] = [
-	preload("res://scenes/objects/pickups/pumpkin_small.tscn"),
-]
+static var rare: Array[String] = []
 static var epic_prefix := "epic_"
 static var epic_chance := 10.0
-static var epic: Array[Resource] = [
-	preload("res://scenes/objects/pickups/pumpkin.tscn"),
-]
-static var all: Array[Resource] = comm + rare + epic
+static var epic: Array[String] = []
 
 enum PickupPool {
 	ALL = 0,
@@ -26,16 +21,32 @@ enum Chances {
 	NONE = 0, GRIM = 1, SLIM = 2, OKAY = 3, GOOD = 4, EASY = 5
 }
 
+static func _static_init():
+	var dir = DirAccess.open(path)
+	if not dir: return
+	
+	for file in dir.get_files():
+		if not file.ends_with(".tscn"):
+			continue
+		var name = file.get_basename()
+		
+		if name.begins_with(comm_prefix):
+			comm.append(name)
+		elif name.begins_with(rare_prefix):
+			rare.append(name)
+		elif name.begins_with(epic_prefix):
+			epic.append(name)
+
 static func _get_random(roll := -1.0, chance_epic := -1.0, chance_rare := -1.0,
-	chance_comm := -1.0, epic_pool: Array[Resource] = epic,
-	rare_pool: Array[Resource] = rare, comm_pool: Array[Resource] = comm) -> Node3D:
+	chance_comm := -1.0, epic_pool: Array[String] = epic,
+	rare_pool: Array[String] = rare, comm_pool: Array[String] = comm) -> Node3D:
 	if chance_epic < 0: chance_epic = epic_chance
 	if chance_rare < 0: chance_rare = rare_chance
 	if chance_comm < 0: chance_comm = comm_chance
-	
+
 	if chance_comm < 0: chance_comm = 100.0 - chance_epic - chance_rare
 	if roll < 0: roll = randf() * 100
-	
+
 	if roll < chance_epic:
 		return get_epic(epic_pool)
 	elif roll < chance_rare + chance_epic:
@@ -47,27 +58,27 @@ static func _get_random(roll := -1.0, chance_epic := -1.0, chance_rare := -1.0,
 
 static func get_random(pool: PickupPool = PickupPool.ALL,
 	chances: Chances = Chances.OKAY, roll := -1.0):
-	
+
 	var comm_pool := comm
 	var rare_pool := rare
 	var epic_pool := epic
-	
+
 	match pool:
 		PickupPool.ALL:
 			pass
 		PickupPool.FOREST:
 			comm_pool = []
-			rare_pool = [preload("res://scenes/objects/pickups/pumpkin.tscn")]
-			epic_pool = [preload("res://scenes/objects/pickups/candle.tscn")]
+			rare_pool = ['rare_pumpkin']
+			epic_pool = ['epic_pumpkin']
 		PickupPool.BENCH:
-			comm_pool = [preload("res://scenes/objects/pickups/candle.tscn")]
-			rare_pool = [preload("res://scenes/objects/pickups/pumpkin_small.tscn")]
-			epic_pool = [preload("res://scenes/objects/pickups/pumpkin.tscn")]
+			comm_pool = ['comm_matches']
+			rare_pool = ['rare_pumpkin']
+			epic_pool = ['epic_pumpkin']
 	
 	var chance_comm := -1
 	var chance_rare := -1
 	var chance_epic := -1
-	
+
 	match chances:
 		Chances.NONE:
 			chance_comm = 0.0
@@ -93,29 +104,27 @@ static func get_random(pool: PickupPool = PickupPool.ALL,
 			chance_comm = -1.0
 			chance_rare = 45.0
 			chance_epic = 20.0
-	
+
 	return _get_random(roll, chance_epic, chance_rare, chance_comm, epic_pool,
 		rare_pool, comm_pool)
 
-static func get_truly_random(pool: Array[Resource] = all) -> Node3D:
+static func get_truly_random(pool: Array[String] = comm + rare + epic) -> Node3D:
 	return get_by_id(pool.pick_random())
 
-static func get_comm(pool: Array[Resource] = comm) -> Node3D:
-	if not len(pool): return null
-	return pool.pick_random().instantiate()
+static func get_comm(pool: Array[String] = comm) -> Node3D:
+	return Pickups.get_by_id(pool.pick_random() if pool else '')
 
-static func get_rare(pool: Array[Resource] = rare) -> Node3D:
-	if not len(pool): return null
-	return pool.pick_random().instantiate()
+static func get_rare(pool: Array[String] = rare) -> Node3D:
+	return get_by_id(pool.pick_random() if pool else '')
 
-static func get_epic(pool: Array[Resource] = epic) -> Node3D:
-	if not len(pool): return null
-	return pool.pick_random().instantiate()
+static func get_epic(pool: Array[String] = epic) -> Node3D:
+	return get_by_id(pool.pick_random() if pool else '')
 
 static func get_by_id(pickup_id: String) -> Node3D:
-	for scene in all:
-		if scene.resource_path.get_file().get_basename() == pickup_id:
-			return 
-			return scene.instantiate()
-	push_error("Pickup not found: ", pickup_id)
-	return null
+	var scene_path = path + pickup_id + ".tscn"
+	if not FileAccess.file_exists(scene_path):
+		push_error("Pickup not found: ", pickup_id)
+		return null
+	
+	var scene = load(scene_path)
+	return scene.instantiate()
