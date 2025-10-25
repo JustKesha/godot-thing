b61ce5a62741c847c460dbb5646c603ae456499f
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-25 14:10:47 +0400

    feat: Add pickup rate to Pickup Interactable

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -1,13 +1,26 @@
 class_name Pickup extends Interactable
 
-@export var item_name := 'matches'
+@export var item_name := 'candle'
 @export var quantity := 1
+@export var pickup_quantity := 99
 @export var allow_overflow := false
 
 func interact(player: PlayerAPI):
-	var overflow := player.inventory.add(item_name, quantity)
+	var overflow := 0
+	var pickup_amount := 0
+	
+	if pickup_quantity > quantity:
+		pickup_amount = quantity
+	else:
+		pickup_amount = pickup_quantity
+		overflow += quantity - pickup_amount
+	
+	overflow += player.inventory.add(item_name, pickup_amount)
 	
 	if overflow == 0 or allow_overflow:
-		queue_free()
+		remove()
 	else:
 		quantity = overflow
+
+func remove():
+	queue_free()
