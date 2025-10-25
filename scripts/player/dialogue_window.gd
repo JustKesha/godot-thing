2b43e33e2e5020af382f243f59fa0ee2f543965d
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-25 04:40:29 +0400

    feat: Implement slow typing for PlayerDialogueWindow

--- scripts/player/dialogue_window.gd
+++ scripts/player/dialogue_window.gd
@@ -7,15 +7,65 @@ var is_active := false:
 	set(value):
 		is_active = value
 		self.visible = is_active
+		if not is_active:
+			is_typing = false
 var message := "":
 	set(value):
 		message = value
 		label.text = value
 		is_active = len(message) > 0
 
-func display(text: String = "...", timeout: float = 2.5):
-	message = text
+@export_group("General")
+@export var default_message := "..."
+@export var default_timeout := 2.5
+
+@export_group("Typing")
+enum TypeDelay { NONE = 0, DRAMATIC = 12, SLOW = 8, NORMAL = 4, FAST = 2, FASTEST = 1 }
+@export var default_type_delay := TypeDelay.NORMAL
+var is_typing := false
+var _type_message := "":
+	set(value):
+		_type_message = value
+		is_typing = len(_type_message) > 0
+		is_active = is_typing
+var _type_delay := 0
+var _type_timer := 0
+var _type_timeout := 0
+
+func display(text: String = default_message, delay: int = default_type_delay,
+	timeout: float = default_timeout):
+	if delay > 0:
+		message = ""
+		_type_message = text
+		_type_delay = delay
+		_type_timeout = timeout
+	else:
+		message = text
+		start_timeout(timeout)
+
+func type(delta: float):
+	if not is_typing: return
+	if _type_timer > 0:
+		_type_timer -= delta
+		return
+	
+	message += _type_message[len(message)]
+	
+	if message == _type_message:
+		is_typing = false
+		start_timeout(_type_timeout)
+	_type_timer = _type_delay
+
+func start_timeout(timeout: float = 2.5):
 	if timeout > 0: timer.start(timeout)
 
-func _on_timer_timeout():
+func stop_timeout():
+	timer.stop()
 	message = ""
+
+func _process(delta: float):
+	if not is_active: return
+	type(delta)
+
+func _on_timer_timeout():
+	stop_timeout()
