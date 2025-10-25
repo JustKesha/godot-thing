89d26a4c5d00ae91f12231d57ff5ef51cb8b1edd
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-25 04:58:41 +0400

    feat: Add more item dialogues

--- scripts/items/matches.gd
+++ scripts/items/matches.gd
@@ -1,4 +1,20 @@
 extends Item
 
 func _use():
+	if player.lantern.light.is_lit:
+		player.dialogue_window.display([
+			"There's no need for that now",
+			"I don't need those right now",
+			"I will need them when I'm out of light",
+			"Later"
+		].pick_random())
+		return false
+	
 	player.lantern.reignite()
+	player.dialogue_window.display([
+		"Back in the light",
+		"My light is back",
+		"The saving light",
+		"Thank god"
+	].pick_random())
+	return true
--- scripts/items/pumpkin.gd
+++ scripts/items/pumpkin.gd
@@ -2,3 +2,10 @@ extends Item
 
 func _use():
 	player.lantern.fuel += 15
+	player.dialogue_window.display([
+		"Tastes gross",
+		"I dont like pumpkins",
+		"Yuck",
+		"Ew"
+	].pick_random())
+	return true
