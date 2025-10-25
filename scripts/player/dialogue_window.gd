5ee7ea6f1cf3f51b347787b313a6dd3c62eb73ec
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-25 04:05:51 +0400

    refactor: Simplify PlayerDialogueWindow

--- scripts/player/dialogue_window.gd
+++ scripts/player/dialogue_window.gd
@@ -11,15 +11,11 @@ var message := "":
 	set(value):
 		message = value
 		label.text = value
+		is_active = len(message) > 0
 
 func display(text: String = "...", timeout: float = 2.5):
-	is_active = true
-	
 	message = text
-	
-	if timeout > 0:
-		timer.stop()
-		timer.start(timeout)
+	if timeout > 0: timer.start(timeout)
 
 func _on_timer_timeout():
-	is_active = false
+	message = ""
