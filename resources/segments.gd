9ef8341abb9bb38c9854ba5813faadc5e597b6ae
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-27 20:29:26 +0400

    feat: Add Rarity.ANY to WorldSegments Resource

--- resources/segments.gd
+++ resources/segments.gd
@@ -1,11 +1,11 @@
 class_name WorldSegments extends Resource
 
-enum Rarity { COMMON = 0, EPIC = 1, RARE = 2 }
+enum Rarity { ANY = -1, COMMON = 0, EPIC = 1, RARE = 2 }
 
 const path := "res://scenes/segments/"
 const extn := ".tscn"
 const data := {
-	"fence_a": { "rarity": Rarity.COMMON },
+	"fence_a": { "rarity": Rarity.ANY },
 	"fence_b": { "rarity": Rarity.RARE },
 	"fence_c": { "rarity": Rarity.RARE },
 }
@@ -17,11 +17,14 @@ static func _static_init():
 	for id in data.keys():
 		var scene: PackedScene = load(path + id + extn)
 		scenes[id] = scene
+		
 		var rarity = data[id]["rarity"] as int
+		if rarity < 0: rarity = Rarity.values()[randi() % Rarity.size()]
 		while len(ids) <= rarity: ids.append([])
 		ids[rarity].append(id)
+	print(ids)
 
-static func get_random() -> WorldSegment:
+static func get_random(rarity: Rarity = -1) -> WorldSegment:
 	return scenes.values().pick_random().instantiate()
 
 static func get_segment(id: String) -> WorldSegment:
