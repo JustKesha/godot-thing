f0a15bf47af7b662262fe70136bf091ce1845727
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-27 17:36:02 +0400

    fix: Temp fix to segment files missing on export
    https://github.com/godotengine/godot/issues/66014#issuecomment-1440261929
    
    TODO Switch later to manually created lists for easier rarity control (segments, items, pickups)

--- resources/items.gd
+++ resources/items.gd
@@ -2,15 +2,22 @@ class_name Items extends Resource
 
 # NOTE When exporting make sure resources from path below are included
 static var path := "res://scenes/objects/items/"
-static var list := _load_names()
+static var list
+
+static func _static_init():
+	list = _load_names()
 
 static func _load_names() -> Array:
-	var result := []
 	var dir = DirAccess.open(path)
-	if dir:
-		for file in dir.get_files():
-			if file.ends_with(".tscn"):
-				result.append(file.get_basename())
+	
+	if not dir: 
+		push_error("Cannot locate or open '" + path + "'.")
+		return []
+	
+	var result := []
+	for file in dir.get_files():
+		if file.ends_with(".tscn.remap") or file.ends_with(".tscn"):
+			result.append(file.trim_suffix(".remap").get_basename())
 	return result
 
 static func get_random() -> Node3D:
--- resources/segments.gd
+++ resources/segments.gd
@@ -1,6 +1,5 @@
 class_name WorldSegments extends Resource
 
-# NOTE When exporting make sure resources from path below are included
 static var path := "res://scenes/segments/"
 static var list := _load_names()
 
@@ -8,19 +7,26 @@ static func _load_names() -> Array:
 	var result := []
 	var dir = DirAccess.open(path)
 	if not dir: return []
+	
 	for file in dir.get_files():
-		if file.ends_with(".tscn"):
-			result.append(file.get_basename())
+		var resource_name = file.get_basename().trim_suffix(".remap")
+		var resource_path = path + resource_name + ".tscn"
+		
+		if ResourceLoader.exists(resource_path):
+			result.append(resource_name)
 	return result
 
 static func get_random() -> Node3D:
+	if list.is_empty():
+		push_error("Segments list is empty")
+		return null
 	var random_name = list[randi() % list.size()]
 	return get_by_name(random_name)
 
 static func get_by_name(segment_name: String) -> Node3D:
 	var segment_path = path + segment_name + ".tscn"
-	if not ResourceLoader.exists(segment_path):
-		push_error("Segment not found at: " + segment_path)
-		return null
-	var scene = load(segment_path)
-	return scene.instantiate()
+	if ResourceLoader.exists(segment_path):
+		var scene = load(segment_path)
+		return scene.instantiate()
+	push_error("Segment not found: " + segment_path)
+	return null
