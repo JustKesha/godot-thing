6c9c9d8d67e98d8a64aa0446c7483d368de1f4b0
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-29 19:20:19 +0400

    feat: Replace player cursor sprite with AnimatedSprite2D

--- scripts/ui/cursor.gd
+++ scripts/ui/cursor.gd
@@ -2,9 +2,9 @@ class_name PlayerCursor extends Control
 
 enum States { HIDDEN = -1, INSPECT = 0 }
 
-@export var sprite: TextureRect
-@export var sprites := [
-	"res://assets/sprites/cursor/inspect.png",
+@export var sprite: AnimatedSprite2D
+@export var animations := [
+	"INSPECT",
 ]
 
 @export var state := States.HIDDEN:
@@ -14,4 +14,4 @@ enum States { HIDDEN = -1, INSPECT = 0 }
 			self.visible = false
 		else:
 			self.visible = true
-			sprite.texture = load(sprites[state])
+			sprite.play(animations[state])
