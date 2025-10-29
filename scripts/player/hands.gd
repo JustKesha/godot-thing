846ce54360871b3776366affa53f9fdfd3a67ecc
kesha <73757143+JustKesha@users.noreply.github.com>
2025-10-29 18:20:14 +0400

    feat: Update player interactions script to use cursor states

--- scripts/player/hands.gd
+++ scripts/player/hands.gd
@@ -2,16 +2,23 @@ class_name PlayerHands extends RayCast3D
 
 @export var player: PlayerAPI
 
+var is_hovered := false
 var hovered: Interactable = null:
 	set(value):
-		if hovered == value: return
+		if hovered == null and is_hovered: pass
+		elif hovered == value: return
+		
 		if hovered != null: hovered.unhover()
 		
 		if value is Interactable:
 			hovered = value
 			hovered.hover()
+			player.cursor.state = player.cursor.States.INSPECT
+			is_hovered = true
 		else:
 			hovered = null
+			player.cursor.state = player.cursor.States.HIDDEN
+			is_hovered = false
 
 func _process(_delta: float):
 	hovered = get_collider()
