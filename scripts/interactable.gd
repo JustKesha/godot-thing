ab8a54d74ce12424922d4b0af8b8305c9ed92829
kesha <73757143+JustKesha@users.noreply.github.com>
2025-11-10 23:36:18 +0400

    feat: Add cooldown to Interactables & general api changes

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,24 +1,67 @@
-class_name Interactable extends StaticBody3D
+class_name Interactable extends Node
 
-var info_title: String:
-	get(): return info_title if info_title else get_info_title()
-var info_desc: String:
-	get(): return info_desc if info_desc else get_info_desc()
+# GENERAL
+func _on_interact(_player: PlayerAPI): pass
+func interact(_player: PlayerAPI):
+	if is_on_cooldown: return
+	_on_interact(_player)
+	if cooldown_seconds > 0: set_on_cooldown()
 
-@export var info_titles: Array[String] = ['']
-@export var info_descs: Array[String] = ['']
+# COOLDOWN
+func _on_cooldown_set(): pass
+func _on_cooldown_end(): pass
+@export_group("Cooldown")
+@export var cooldown_timer: Timer
+@export var cooldown_seconds: float = .5
+@export var cooldown_enabled: bool = true
+var is_on_cooldown: bool = false:
+	set(value):
+		if value == is_on_cooldown: return
+		is_on_cooldown = value
+		if is_on_cooldown: _on_cooldown_set()
+		else: _on_cooldown_end()
 
-func hover(): pass
-func get_info_title() -> String:
+func set_on_cooldown(time: float = cooldown_seconds):
+	if not cooldown_enabled: return
+	cooldown_timer.start(time)
+	is_on_cooldown = true
+func reset_cooldown():
+	cooldown_timer.stop()
+	is_on_cooldown = false
+
+func _on_cooldown_timer_timeout():
+	is_on_cooldown = false
+
+# READY / REMOVE
+func _on_interactable_ready(): pass
+func _on_remove(): pass
+func _ready(): _on_interactable_ready()
+func remove():
+	_on_remove()
+	queue_free()
+
+# ONHOVER
+func _on_hover(): pass
+func _on_unhover(): pass
+func hover(): _on_hover()
+func unhover(): _on_unhover()
+
+# INFO POPUP
+@export_group("Info Popup")
+# Option 1: Set a const title / desc
+@export var info_title: String:
+	get(): return info_title if info_title else _get_info_title()
+@export var info_desc: String:
+	get(): return info_desc if info_desc else _get_info_desc()
+
+# Option 2: Fill in multiple values to switch between them randomly
+@export var info_titles: Array[String] = []
+@export var info_descs: Array[String] = []
+
+# Option 3: Use custom getters
+func _get_info_title() -> String:
 	if not info_titles: return ''
 	return info_titles.pick_random()
-func get_info_desc() -> String:
+func _get_info_desc() -> String:
 	if not info_descs: return ''
 	return info_descs.pick_random()
-func unhover(): pass
-
-func interact(_player: PlayerAPI): pass
-
-func _interactable_ready(): pass
-func _ready(): _interactable_ready()
-func remove(): queue_free()
