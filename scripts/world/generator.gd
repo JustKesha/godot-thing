1fcfc7c57f413b345b921a1a1594a18eb6b9f26a
kesha <KESHApopolzen@yandex.ru>
2025-12-08 09:28:39 +0400

    refactor: Cleanup WorldGenerator
    Minor fixes and refactoring

--- scripts/world/generator.gd
+++ scripts/world/generator.gd
@@ -4,13 +4,14 @@ class_name WorldGenerator extends Node
 
 @export_group("Segmentation")
 @export var preload_segments := 10
-@export var segment_spacing := 10.0
-@export var segment_unload_at_z := 20.0
-var loaded_segments: Array[Node3D] = []
+@export var segments_spacing := 10.0
+@export var segments_behind := 2
+var loaded_segments: Array[WorldSegment] = []
 var current_segment: WorldSegment
 
 @export_group("Horizon")
-@export var horizon_start_at_segment := 1
+@export var horizon_enabled := true
+@export var horizon_start_at_segment := 3
 @export var horizon_depth := .1
 
 ## Clears the map and loads up a new one.
@@ -18,34 +19,12 @@ func init():
 	clear()
 	update()
 
-## Loads new segments and unloads old ones when needed.
+## Loads new segments and unloads passed ones.
 func update():
 	clean()
 	while len(loaded_segments) < preload_segments:
-		load_segment(get_next_segment_to_load())
-
-## Removes all loaded segments.
-func clear() -> int:
-	var segments_removed := 0
-	for segment in loaded_segments:
-		if is_instance_valid(segment):
-			segment.queue_free()
-			segments_removed += 1
-	loaded_segments.clear()
-	return segments_removed
-
-## Unloades all passed segments.
-func clean() -> int:
-	var segments_to_remove: Array[Node3D] = []
-	
-	for segment in loaded_segments:
-		if is_segment_unload_time(segment):
-			segments_to_remove.append(segment)
-	
-	for segment in segments_to_remove:
-		unload_segment(segment)
-	
-	return len(segments_to_remove)
+		load_segment()
+	current_segment = get_current_segment()
 
 ## Moves all segments along the z axis.
 func move(by: float):
@@ -55,19 +34,46 @@ func move(by: float):
 			segment.position.y = clamp(
 				segment.position.y + by * horizon_depth, -INF, 0)
 	update()
-	current_segment = get_current_segment()
+
+## Unloades all passed segments, returns the number of unloaded segments.
+func clean() -> int:
+	var segments_to_unload: Array[Node3D] = []
+	
+	# TODO OPTIMIZE: Segments should always be loaded by load_segment()
+	# -> thfor 
+	for segment in loaded_segments:
+		if is_segment_unload_time(segment):
+			segments_to_unload.append(segment)
+	
+	for segment in segments_to_unload:
+		unload_segment(segment)
+	
+	return len(segments_to_unload)
+
+## Unloads all loaded segments.
+func clear() -> int:
+	var segments_removed := 0
+	for segment in loaded_segments:
+		if is_instance_valid(segment):
+			segment.queue_free()
+			segments_removed += 1
+	loaded_segments.clear()
+	return segments_removed
 
 # ACTIONS
 
-func load_segment(segment: WorldSegment):
-	if not segment is WorldSegment:
-		push_error("Trying to load a null segment.")
-		return
+## Loads a segment into the world, if given null will use get_next_segment_to_load.
+func load_segment(segment: WorldSegment = null):
+	while segment == null:
+		segment = get_next_segment_to_load()
 	world.add_child(segment)
 	segment.position = get_next_segment_position()
 	loaded_segments.append(segment)
 
-func unload_segment(segment: WorldSegment):
+## Unloads given segment, if given null will unload the oldest segment.
+func unload_segment(segment: WorldSegment = null):
+	if segment == null:
+		segment = loaded_segments[0]
 	if is_instance_valid(segment):
 		segment.queue_free()
 		loaded_segments.erase(segment)
@@ -75,28 +81,35 @@ func unload_segment(segment: WorldSegment):
 # HELPERS
 
 func get_next_segment_position() -> Vector3:
-	var index = get_next_segment_index()
-	var pos = Vector3.FORWARD * index * segment_spacing
-	if index >= horizon_start_at_segment:
-		pos.y = ((index - horizon_start_at_segment) * segment_spacing
-			* -horizon_depth)
-	return pos
+	var last_z = (
+		loaded_segments[-1].position.z if loaded_segments
+		else (segments_behind + 1) * segments_spacing )
+	var pos = Vector3(0, 0, last_z - segments_spacing)
 
-func get_next_segment_index() -> int:
-	return len(loaded_segments) - 1
+	if horizon_enabled and len(loaded_segments) >= horizon_start_at_segment:
+		pos.y = ( (len(loaded_segments) - horizon_start_at_segment)
+			* segments_spacing * -horizon_depth )
+	
+	return pos
 
 func get_next_segment_to_load() -> WorldSegment:
 	return WorldSegments.roll()
 
-func is_segment_unload_time(segment: WorldSegment = loaded_segments[0]) -> bool:
-	return segment.position.z > segment_unload_at_z
+func is_segment_unload_time(segment: WorldSegment) -> bool:
+	return segment.position.z > (segments_behind + 1) * segments_spacing
 
 func get_current_segment() -> WorldSegment:
+	if len(loaded_segments) >= segments_behind:
+		return loaded_segments[segments_behind]
+	
+	# NOTE Fallback manual check, should never happen
 	var player_z := References.player.body.global_position.z
+	
 	for segment in loaded_segments:
-		if( segment.position.z + segment_spacing/2 > player_z
-			and segment.position.z - segment_spacing/2 < player_z ):
+		if( segment.position.z + segments_spacing/2 >= player_z
+			and segment.position.z - segments_spacing/2 < player_z ):
 			return segment
+	
 	return null
 
 # GENERAL
