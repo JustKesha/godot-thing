abc1dcb1c49d985f0e7d50ef8c87c1c33e42054a
kesha <KESHApopolzen@yandex.ru>
2026-01-08 08:54:31 +0400

    refactor: Improve exports & docs for Observer & ObserverAI

--- scripts/npcs/observer.gd
+++ scripts/npcs/observer.gd
@@ -1,11 +1,16 @@
-class_name Observer extends Node3D
+class_name Observer extends Node
+## Can track [Node3D] type objects with [member head] & [member eyes], as well as
+## change [member pupil_size].
 
-@export_group("Head")
-@export var head: Node3D
-@export var head_turn_rate: float = 1.0
+@export_group("Body")
 
-@export_group("Eyes")
-@export var eyes: Array[Eye] = []
+@export_subgroup("Head")
+@export var head: Node3D ## [Observer]'s head.
+@export var head_turn_rate: float = 1.0 ## [member head]'s turn rate.
+
+@export_subgroup("Eyes")
+@export var eyes: Array[Eye] = [] ## [Observer]'s eyes.
+## Sets [member Eye.turn_rate] for each of [Observer]'s [member eyes].
 @export var eyes_turn_rate: float = 1.0:
 	set(value):
 		eyes_turn_rate = value
@@ -13,14 +18,18 @@ class_name Observer extends Node3D
 			if not eye: continue
 			eye.turn_rate = eyes_turn_rate
 enum PupilSize { RELAXED = 130, NORMAL = 75, ALERT = 50, HAUNT = 0 }
-@export var pupils_size: PupilSize = PupilSize.NORMAL:
+## Sets [member Eye.pupil_size] for each of [Observer]'s [member eyes].
+@export var pupil_size: PupilSize = PupilSize.NORMAL:
 	set(value):
-		pupils_size = value
+		pupil_size = value
 		for eye in eyes:
 			if not eye: continue
-			eye.set_pupil_size(float(pupils_size))
+			eye.set_pupil_size(float(pupil_size))
 
 @export_group("Tracking")
+## The object that [Observer]'s [member head] and [member eyes] are currently tracking.
+## [br][br]NOTE: [member head] is turned using [method _turn] but [member eyes]
+## logic is covered in a separate script.
 @export var target_object: Node3D:
 	set(value):
 		target_object = value
@@ -28,13 +37,19 @@ enum PupilSize { RELAXED = 130, NORMAL = 75, ALERT = 50, HAUNT = 0 }
 			if not eye: continue
 			eye.follow(target_object)
 
-func follow(object: Node3D):
+## Start tracking a [Node3D] and set it as [member target_object].
+func track(object: Node3D):
 	target_object = object
 
-func stop():
+## Stop tracking the [member target_object].
+func stop_tracking():
 	target_object = null
 
-# NOTE _ready is over-written in ObserverAI
-func _physics_process(delta: float):
+## Slowly turns the [member head] to face [member target_object].
+func _turn(delta: float = -1):
 	if not head or not target_object: return
+	if delta < 0: delta = get_process_delta_time()
 	Util.turn(head, target_object.global_position, head_turn_rate * delta)
+
+func _physics_process(delta: float):
+	_turn(delta)
--- scripts/npcs/observer_ai.gd
+++ scripts/npcs/observer_ai.gd
@@ -1,54 +1,86 @@
 class_name ObserverAI extends Observer
+## Basic AI for [Observer] that scans the surroundings for new things to lock its gaze onto.
 
-@onready var player: PlayerAPI = References.player
-@export var interest_area: Area3D
-@export var update_timer: Timer
+@onready var player := References.player ## The player reference.
 
+@export_group("Scan", "scan_")
+## [method ObserverAI.scan] will be called on every [signal Timer.timeout].
+@export var scan_timer: Timer:
+	set(value):
+		scan_timer = value
+		scan_timer.connect('timeout', scan)
+## The [Area3D] in which [ObserverAI] will look for triggers.
+## [br][br]NOTE: Some [enum TriggerType]s are global and don't require an [Area3D].
+@export var scan_area: Area3D
+
+@export_group("Triggers", "triggers_")
+## Determens the objects that [Observer] will be locked onto and the conditions.
+enum TriggerType {
+	FUEL, ## Tracks the player when fuel is low, changes pupil size based on fuel level.
+	DICE, ## Tracks nearby rolling dice.
+	INTERACTABLES, ## Tracks Interactables hovered by the player.
+	INVENTORY, ## Tracks items held by the player.
+	PLAYER ## Tracks the player.
+	}
+## The order in which the [enum TriggerType]s are checked every [method scan] call.
+@export var triggers_priority: Array[TriggerType] = [ 
+	TriggerType.FUEL,
+	TriggerType.DICE,
+	TriggerType.INTERACTABLES,
+	TriggerType.INVENTORY,
+	TriggerType.PLAYER
+	]
+## Allows some [enum TriggerType]s to set [member Observer.pupil_size].
+@export var triggers_can_change_pupil: bool = true
+
+## Looks for possbile triggers and updates [member Observer.target_object] if one found.
 func scan():
 	var trigger = get_trigger()
 	if not trigger: return
 	target_object = trigger
 
+## Returns a [Node3D] that [ObserverAI] is currently most interested in or [code]null[/code].
 func get_trigger() -> Node3D:
-	var bodies = interest_area.get_overlapping_bodies() if interest_area else []
-	var fuel_percentage = player.lantern.fuel_percentage
-	
-	# Player
-	if not player.lantern.is_lit:
-		pupils_size = PupilSize.HAUNT
-		return player.head
-		
-	# Player
-	elif fuel_percentage <= 20:
-		pupils_size = PupilSize.ALERT
-		return player.head
-		
-	elif fuel_percentage <= 60:
-		pupils_size = PupilSize.NORMAL
-	else:
-		pupils_size = PupilSize.RELAXED
-	
-	# Rolling Dice
-	for trigger in bodies:
-		if not trigger: continue
-		if trigger is Dice and trigger.is_rolling:
-			return trigger.body
-	
-	# Hovered Interactables
-	if player.eyes.hovered:
-		pupils_size = PupilSize.RELAXED
-		return player.eyes.hovered
-	
-	# Held Items
-	if player.inventory.is_open:
-		pupils_size = PupilSize.RELAXED
-		return player.inventory
-	
-	# Player
-	return player.head
+	for trigger_type in triggers_priority:
+		var object = get_trigger_by_type(trigger_type)
+		if object: return object
+	return null
 
-func _ready():
-	if update_timer: update_timer.connect('timeout', _on_update_timer_timeout)
+## Returns a [Node3D] for specific [enum TriggerType] that [ObserverAI] is
+## currently most interested in or [code]null[/code].
+func get_trigger_by_type(type: TriggerType) -> Node3D:
+	match type:
+		TriggerType.FUEL:
+			var fuel_percentage = player.lantern.fuel_percentage
+			if not player.lantern.is_lit:
+				if triggers_can_change_pupil: pupil_size = PupilSize.HAUNT
+				return player.head # INFO Trigger: Player
+			elif fuel_percentage <= 20:
+				if triggers_can_change_pupil: pupil_size = PupilSize.ALERT
+				return player.head # INFO Trigger: Player
+			elif fuel_percentage <= 60:
+				if triggers_can_change_pupil: pupil_size = PupilSize.NORMAL
+			else:
+				if triggers_can_change_pupil: pupil_size = PupilSize.RELAXED
+		TriggerType.DICE:
+			var bodies = get_area_bodies()
+			for trigger in bodies:
+				if not trigger: continue
+				if trigger is Dice and trigger.is_rolling:
+					return trigger.body # INFO Trigger: Rolling Dice
+		TriggerType.INTERACTABLES:
+			if player.eyes.hovered:
+				if triggers_can_change_pupil: pupil_size = PupilSize.RELAXED
+				return player.eyes.hovered # INFO Trigger: Hovered Interactable
+		TriggerType.INVENTORY:
+			if player.inventory.is_open:
+				if triggers_can_change_pupil: pupil_size = PupilSize.RELAXED
+				return player.inventory # INFO Trigger: Held Item
+		TriggerType.PLAYER:
+			return player.head # INFO Trigger: Player
+	return null
 
-func _on_update_timer_timeout():
-	scan()
+## Returns the result of [method Area3D.get_overlapping_bodies] if
+## [member scan_area] is provided else [code][][/code].
+func get_area_bodies() -> Array[Node3D]:
+	return scan_area.get_overlapping_bodies() if scan_area else []
