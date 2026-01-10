f9c941574b60e39f0841eddb6be056484162542a
kesha <KESHApopolzen@yandex.ru>
2026-01-10 20:36:48 +0400

    fix: Minor Pickups fix and documentation

--- resources/pickups.gd
+++ resources/pickups.gd
@@ -1,4 +1,5 @@
 class_name Pickups extends Resource
+## Stores data about all [Pickup]s and provides access methods.
 
 enum Rarity { ANY = -1, COMMON, RARE, EPIC }
 enum Pool { ALL, FOREST, STRUCTURE }
@@ -49,6 +50,15 @@ const DATA := {
 		},
 	}
 
+## Automatically generated at [method _static_init]. Follows the structure: [br]
+## [code]pools = {
+##     Pool.TYPE: {
+##         Rarity.COMMON: ["pickup_id", ...],
+##         Rarity.RARE: [...],
+##         Rarity.EPIC: [...],
+##     }, ...
+## }[/code][br]NOTE: All [Pickup]s are automatically assigned to [member Pool.ALL]
+## in addition to their custom pools from [member DATA].
 static var pools: Dictionary[Pool, Dictionary] = {}
 
 static func _generate_loot_pools():
@@ -69,6 +79,8 @@ static func _static_init():
 
 # ACTIONS
 
+## Get random [Pickup] of certain rarity from given [param pool] or [code]null[/code]
+## is nothing found. If [param rarity] < 0 ([member Rarity.ANY]) will use random [member Rarity].
 static func get_random(pool: Pool = Pool.ALL,
 	rarity: Rarity = Rarity.ANY) -> Pickup:
 	var pickup_ids := get_pickup_ids(pool, rarity)
@@ -76,6 +88,7 @@ static func get_random(pool: Pool = Pool.ALL,
 	
 	return get_by_id(pickup_ids.pick_random())
 
+## Get a [Pickup] instance by an ID [String] or [code]null[/code] if nothing found.
 static func get_by_id(pickup_id: String) -> Pickup:
 	var pickup_path := PATH + pickup_id + EXTN
 	if not ResourceLoader.exists(pickup_path):
@@ -88,13 +101,19 @@ static func get_by_id(pickup_id: String) -> Pickup:
 	
 	return pickup
 
+## Returns a random [Pickup] from given [param pool] if roll [param percent] (default 0.0-100.0) and
+## [param chances] (luck) were high enough (see [method get_rarity_by_roll]) otherwise returns [code]null[/code].
 static func get_by_roll(pool: Pool = Pool.ALL,
 	chances: Chances = Chances.OKAY, percent: float = -1) -> Pickup:
 	var rarity = get_rarity_by_roll(chances, percent)
+	if rarity < 0: return null
+	
 	return get_random(pool, rarity)
 
 # HELPERS
 
+## Get list of [Pickup] ID [String]s for specific [enum Pool] and [enum Rarity].
+## If [param rarity] < 0 ([member Rarity.ANY]) will use random [enum Rarity].
 static func get_pickup_ids(pool: Pool = Pool.ALL,
 	rarity: Rarity = Rarity.ANY) -> Array:
 	if rarity < 0:
@@ -105,6 +124,8 @@ static func get_pickup_ids(pool: Pool = Pool.ALL,
 		].pick_random()
 	return pools[pool][rarity] as Array
 
+## Get percent chance for a [Pickup] of certain [enum Rarity] appearing with given luck
+## ([param chances]). If [param rarity] < 0 ([member Rarity.ANY]) will use random [enum Rarity].
 static func get_rarity_chance(rarity: Rarity = Rarity.ANY,
 	chances: Chances = Chances.OKAY) -> float:
 	if rarity < 0:
@@ -115,6 +136,10 @@ static func get_rarity_chance(rarity: Rarity = Rarity.ANY,
 		].pick_random()
 	return CHANCES[chances][rarity] as float
 
+## Get [Pickup] [enum Rarity] from a roll [param percent] and luck ([param chances]).
+## The higher the [param chances] the less roll [param percent] you need to get a higher
+## [enum Rarity]. By default [param percent] is a random [float] from 0 to 100.
+## NOTE: Will return [member Rarity.ANY] (-1) if no other [enum Rarity] could be achieved.
 static func get_rarity_by_roll(chances: Chances = Chances.OKAY,
 	percent: float = -1) -> Rarity:
 	if percent < 0: percent = randf() * 100
